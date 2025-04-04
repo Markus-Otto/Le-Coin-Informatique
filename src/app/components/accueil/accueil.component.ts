ccb79d755d223d80063f44abd2b74f1f64e56b95
Markus-Otto <tommanlay4@gmail.com>
2025-04-04 15:28:44 +0200

    V2.1 mise a jour header + hero

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ScrollService } from '../../services/scroll.service'; // Importation du service de défilement
+import { CommonModule } from '@angular/common'; // Importation du module commun pour les directives Angular
 
 @Component({
   selector: 'app-accueil',
@@ -8,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
+  // Injection du service dans le constructeur
+  constructor(private scrollService: ScrollService) {}
 
+  // Méthode appelée lors du clic sur un bouton de défilement
+  scrollToSection(sectionId: string): void {
+    this.scrollService.scrollToComponent(sectionId);  // Appelle la méthode du service
+  }
 }
