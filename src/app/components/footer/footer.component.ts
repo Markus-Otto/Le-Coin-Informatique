9eb375d9f8e633e75c5b45b0bacbf2ec160a57a3
Markus-Otto <tommanlay4@gmail.com>
2025-04-04 16:59:41 +0200

    V3 header + hero fonctionelle

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,23 @@
+// footer.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importation du module commun pour les directives Angular
+import { ScrollService } from '../../services/scroll.service'; // Importation du service de défilement
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Ajout du module commun pour les directives Angular
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  isModalOpen = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
